fbd8041fce56dd63dc58cf2b5464fd4e365eef4f
Yash-hacker-tech <y.sanghi2007@gmail.com>
2025-10-14 15:01:54 +0530

    14-10-2025

---
+++ CF2155A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define ll long long
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define vvc vector<vector<char>>
+#define vpii vector<pair<int, int>>
+#define umii unordered_map<int, int>
+#define umll unordered_map<long long, long long>
+#define r(i, s, e) for (int i = s; i < e; i++)
+#define r0(i, e, s) for(int i = e; i >= s; i--)
+#define maxInVec(v) *max_element(v.begin(), v.end())
+#define minInVec(v) *min_element(v.begin(), v.end())
+#define maxIdxInVec(v) max_element(v.begin(), v.end()) - v.begin()
+#define minIdxInVec(v) min_element(v.begin(), v.end()) - v.begin()
+#define MOD 100000007
+
+using namespace std;
+
+bool isValidTriangle(ll a, ll b, ll c) 
+{
+    return (a + b > c) && (a + c > b) && (b + c > a);
+}
+
+
+void solve() 
+{
+    ll n;
+    cin >> n;
+    cout << n * 2 - 2 << "\n";
+    return;
+}
+
+int main()
+{
+    fast;
+    ll t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
