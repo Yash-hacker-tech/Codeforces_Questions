e2707641b33de45f435aae494d166591216398cd
Yash-hacker-tech <y.sanghi2007@gmail.com>
2025-10-21 23:59:32 +0530

    modifying MainLine

--- MainLine.cpp
+++ MainLine.cpp
@@ -30,6 +30,19 @@ bool isValidTriangle(ll a, ll b, ll c)
     return (a + b > c) && (a + c > b) && (b + c > a);
 }
 
+bool isPrime(ll n) 
+{
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    
+    for (ll i = 5; i * i <= n; i += 6) 
+    {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
 
 void solve() 
 {
